ae19c64492119751eb6719df4bb1b5de19daee30
se7enrains <yan.krytski@gmail.com>
2020-01-28 10:44:46 +0300

    added config files support (fails on readFile with IOException)

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <libconfig.h++>
 
+using namespace libconfig;
 using namespace boost::asio;
 using ip::tcp;
 using std::string;
@@ -20,6 +22,27 @@ void sendSocket(tcp::socket &socket, const string &message){
 }
 
 int main() {
+    Config cfg;
+    try{
+        cfg.readFile("server.cfg");
+    }
+    catch (const FileIOException &fioex){
+        std::cerr << "I/O error while reading file" << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const ParseException &pex){
+        std::cerr << "Parse exception at " << pex.getFile() << ":" << pex.getLine()
+                  << " - " << pex.getError() << std::endl;
+        return EXIT_FAILURE;
+    }
+    try{
+        string ip = cfg.lookup("server.ip");
+        std::cout << "server.ip = " << ip << std::endl;
+    }
+    catch(const SettingNotFoundException &snfec){
+        std::cerr << "No \"ip\" value in configuration file" << std::endl;
+        return EXIT_FAILURE;
+    }
     io_service ioService;
     tcp::acceptor acceptor(ioService, tcp::endpoint(tcp::v4(), 61324));
     tcp::socket  socket(ioService);
